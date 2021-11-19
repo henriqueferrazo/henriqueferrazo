dda5d48f984cd40799e264a3e6d9c63f33c1f2fb
unknown <heriqueoliver9@gmail.com>
2021-11-18 23:46:19 -0300

    primeiro repositorio

---
+++ variavel.js
@@ -0,0 +1,59 @@
+
+/**
+//  *
+ */
+
+// var alimentos= prompit("adcione os produto");
+// var ValorAnterior= 10,50;
+// var ValorAtual= 20,50;
+
+// if (alimento="tomate" && "laranja" && "limão" && "abacaxi" ==(valorAnterior= 10,50)){
+// console.log("Valor anterior")
+// }else if (alimento="tomate" && "laranja" && "limão" && "abacaxi" ==(ValorAtual= 20,50)){
+//     console.log("Valor atual")
+// }
+
+
+// function dirigirEhBeber(num){
+//    var resultado = num >= 18;
+//     (if num > 18) return resultado;
+// }else 
+
+// function previsaoRodagem (litro, media){
+//     alert ("É possivel rodar " + litro * media + " km");
+// }
+// previsaoRodagem(prompt("declare os litros"), prompt("declare os km"));
+
+// var precoKgArroz= 7,53;
+// var qtdArroz= 3;
+
+// var gastoTotal=  precoKgArroz * qtdArroz
+ 
+// var email= prompt ("insira seu email")
+
+// function validaEmail(email);{
+//     if (email.indexOf("@") != -1){
+//         return true;
+//     }els
+//         return false
+//     }
+//  }
+// pr
+
+function listaDecrescente(numero);
+    while(numero > 0);
+    document.write(`número = ${numero}<br>`);
+    numero--;
+}listaDecrescente(20);
+
+
+function fizz(numero);
+    var base = 0;
+    while(base < numero);
+        if(base == 1);{
+            document.write("numero = fizz");
+    }else{
+        document.write(prompt(`numero = ${base}`));
+        }
+        base++;
+    }
